f9350c971d4548b96dabc56ffb5e40de1cafc074
git-create-devben <DevBen.tech@outlook.com>
2023-12-01 22:06:57 +0100

    update

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,16 +1,18 @@
-import Spline from '@splinetool/react-spline';
+"use client";
 
+import Spline from "@splinetool/react-spline";
 const Hero = () => {
-  
-
- 
-
   return (
-    <div>
-        <h1 className='text-4xl text-red-800 p-11'>Hello Creator</h1>
-           <Spline scene="https://prod.spline.design/nm5198Fh39lOIZG5/scene.splinecode" />
+    <div className="relative">
+      <div className="absolute top-9">
+        <h1 className="text-4xl text-red-800 p-11">Hello Creator</h1>
+      </div>
+      <Spline
+        className="hidden"
+        scene="https://prod.spline.design/nm5198Fh39lOIZG5/scene.splinecode"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;

8889469cb5e1ddef6ee143a3a8c8f3e2c241cec2
git-create-devben <DevBen.tech@outlook.com>
2023-12-03 15:23:02 +0100

    change the 3d text

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -3,14 +3,16 @@
 import Spline from "@splinetool/react-spline";
 const Hero = () => {
   return (
-    <div className="relative">
-      <div className="absolute top-9">
-        <h1 className="text-4xl text-red-800 p-11">Hello Creator</h1>
+    <div className="relative hidden">
+      <div className="h-[12rem] w-full relative bg-red-400">
+        <Spline
+          className="absolute inset-0 w-full h-full"
+          scene="https://prod.spline.design/fMDKTJ-OX2OJ8y7x/scene.splinecode"
+        />
+        <div className="absolute top-9 left-0 right-0 text-center">
+          <h1 className="text-4xl text-red-800">Hello Creator</h1>
+        </div>
       </div>
-      <Spline
-        className="hidden"
-        scene="https://prod.spline.design/nm5198Fh39lOIZG5/scene.splinecode"
-      />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import Hero from "@/Hero";
+"use client";
 
-export default function Page() {
-  return <div>
-    <Hero/>
-  </div>;
+import Spline from "@splinetool/react-spline";
+
+export default function Page() {  
+  return (
+    <div className="h-full w-full bg-black">
+      <div className="relative hidden">
+        <div className="h-[12rem] w-full relative bg-red-400">
+          <Spline
+            className="absolute inset-0 w-full h-full"
+            scene="https://prod.spline.design/fMDKTJ-OX2OJ8y7x/scene.splinecode"
+          />
+          <div className="absolute top-9 left-0 right-0 text-center">
+            <h1 className="text-4xl text-red-800">Hello Creator</h1>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-4xl text-red-900">Hello Page</h1>
+    </div>
+  );
 }

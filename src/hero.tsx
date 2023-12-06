ec36e8be5b1d3c64ee0d167069c5a56aa2f2b219
git-create-devben <DevBen.tech@outlook.com>
2023-12-06 01:35:00 +0100

    add tailwind nesting

--- src/hero.tsx
+++ src/hero.tsx
@@ -1,8 +1,10 @@
+"use client"
+import "./app/globals.css"
 import Spline from '@splinetool/react-spline';
 const hero = () => {
   return (
-    <div>
-            <Spline scene="https://prod.spline.design/ZBQTTfn5dKRfPOxz/scene.splinecode" />
+    <div className='relative wrapper'>
+       <Spline className='absolute top-0 right-0 m-0 spline' scene="https://prod.spline.design/ZBQTTfn5dKRfPOxz/scene.splinecode" />
         <h1 className="text-red-400 text-4xl">Hello world</h1>
     </div>
   )

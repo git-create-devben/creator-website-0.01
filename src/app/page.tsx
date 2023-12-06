192596a9a9bee7fc2c3af9822bea9132ac31c45b
git-create-devben <DevBen.tech@outlook.com>
2023-12-06 07:38:11 +0100

    delete

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "../hero";
-export default function Home() {
-  return (
-    <main className="">
-      {/* <h1 className="text-4xl text-red-400">hello</h1> */}
-      <Hero />
-    </main>
-  );
-}

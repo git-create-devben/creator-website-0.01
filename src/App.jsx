83bbf627050a16b43ec1e6ad27286aefd7aeff4f
git-create-devben <DevBen.tech@outlook.com>
2023-12-06 07:50:30 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Spline from "@splinetool/react-spline"
 export default function App(){
   return (
     <div className="relative">
       <div>
+      <Spline className="spline" scene="https://prod.spline.design/ZBQTTfn5dKRfPOxz/scene.splinecode" />
         <h1 className="text-4xl text-red-400">Hello</h1>
       </div>
     </div>

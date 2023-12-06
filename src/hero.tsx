d9b90c14e668c7f00b3d8f4abaafacc5ea8730cc
git-create-devben <DevBen.tech@outlook.com>
2023-12-06 01:11:21 +0100

    install spline

---
+++ src/hero.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+const hero = () => {
+  return (
+    <div>
+            <Spline scene="https://prod.spline.design/ZBQTTfn5dKRfPOxz/scene.splinecode" />
+        <h1 className="text-red-400 text-4xl">Hello world</h1>
+    </div>
+  )
+}
+
+export default hero

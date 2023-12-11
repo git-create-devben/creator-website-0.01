9f374acadf538c8802508c7e929b8d327e625571
git-create-devben <DevBen.tech@outlook.com>
2023-12-11 11:59:31 +0100

    add tailwind

---
+++ src/gallery/gallery.tsx
@@ -0,0 +1,10 @@
+
+const gallery = () => {
+  return (
+    <main>
+        <h1 className="text-white">hello</h1>
+    </main>
+  )
+}
+
+export default gallery
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Gallery from "./gallery/gallery.tsx"
+import ErrorPage from "./error.tsx"
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage />,
+  },
+
+  {
+    path: "/Gallery",
+    element: <Gallery/>,
+  },
+  
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>,
 )

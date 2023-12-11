35c882481cb3128b99f62304479551d203feab7a
git-create-devben <DevBen.tech@outlook.com>
2023-12-11 12:35:33 +0100

    initializing shadcdn

--- src/gallery/gallery.tsx
+++ src/gallery/gallery.tsx
@@ -1,8 +1,21 @@
-
+import img from "../assets/bg.png"
 const gallery = () => {
   return (
-    <main>
-        <h1 className="text-white">hello</h1>
+    <main className="p-20 flex justify-center h-[100vh] w-full">
+        <div className="flex flex-col gap-20">
+            <div className="flex flex-row items-center justify-center"> 
+              <img src={img} alt=""  className="rounded-full w-52 "/>
+                <div className="flex flex-col gap-20px">
+                 <div className="flex flex-row">
+                  <h2>Christinadmrtzi</h2>
+                  
+                 </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
     </main>
   )
 }

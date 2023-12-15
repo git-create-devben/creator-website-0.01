a728aec7400161f9e8cd604644e62c85dfdcb799
git-create-devben <DevBen.tech@outlook.com>
2023-12-15 02:01:53 +0100

    update

--- AnimRoute.jsx
+++ AnimRoute.jsx
@@ -4,19 +4,48 @@ import Gallery from "./src/pages/Gallery/Gallery";
 import Service from "./src/pages/services/service";
 import Contact from "./src/pages/contact/contact";
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./src/index.css";
 
 const AnimRoute = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous operation (e.g., fetching data)
+    const fetchData = async () => {
+      // Your async code here
+
+      // Simulate a delay (remove this in your actual implementation)
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      // Set loading to false once data is fetched
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
   return (
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/Gallery" element={<Gallery />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/contact" element={<Contact />} />
-     </Routes>
+    <div>
+      {isLoading ? (
+        // Show a loading indicator (you can replace this with your loading component)
+        <div className="loader">
+          <div className="intern"></div>
+          <div className="external-shadow">
+            <div className="central"></div>
+          </div>
+        </div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      )}
+    </div>
   );
-}
-
-
+};
 
 export default AnimRoute;
--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,41 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimRoute from "../AnimRoute";
 import Header from "./components/Header/header";
+import { useState, useEffect } from "react";
+import "./index.css"
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous operation (e.g., fetching data)
+    const fetchData = async () => {
+      // Your async code here
+
+      // Simulate a delay (remove this in your actual implementation)
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      // Set loading to false once data is fetched
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
+       {isLoading ? (
+        // Show a loading indicator (you can replace this with your loading component)
+        <div className="loader">
+          <div className="intern"></div>
+          <div className="external-shadow">
+            <div className="central"></div>
+          </div>
+        </div>
+      ) : (
       <Router>
-        <Header/>
+        <Header />
         <AnimRoute />
       </Router>
+      )}
     </>
   );
 }

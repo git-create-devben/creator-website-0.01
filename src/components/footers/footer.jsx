894145859ace9939f444a17f133a2c067f237132
git-create-devben <DevBen.tech@outlook.com>
2023-12-15 18:41:22 +0100

    update footer and fixed some bugs

--- src/components/footers/footer.jsx
+++ src/components/footers/footer.jsx
@@ -4,13 +4,13 @@ const footer = () => {
     <section className="flex flex-wrap items-center justify-around">
       <div className="grid grid-cols-2  place-content-center items-center text-white p-4">
         <span className="text-md text-white ">
-          <small className="text-[1.2rem] text-white opacity-50">Mail:</small>
-          <i className="text-1xl text-white opacity-100 ml-2">
+          <small className="lg:text-[1.2rem] text-white opacity-50">Mail:</small>
+          <i className="lg:text-1xl text-sm text-white opacity-100 ml-2">
             Christina@gmail.com
           </i>
         </span>
         <span>
-          <small className="text-[1.1rem] text-white opacity-50 ml-2">
+          <small className="lg:text-[1.1rem] text-white opacity-50 ml-2">
             Phone:
           </small>
           <i className="text-[0.840rem] text-white opacity-100 ml-2">
@@ -19,7 +19,7 @@ const footer = () => {
         </span>
       </div>
       <hr className="w-24 text-white opacity-50 lg:flex hidden" />
-      <div className="text-white flex flex-wrap">
+      <div className="text-white flex flex-wrap lg:text-1xl text-sm">
         <span>&copy; Christina 2023. </span>
         <span>
           Design &amp; developed by{" "}
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,6 +1,7 @@
 // import Carousel from "./Slider";
 import "./home.css";
 // import Footer from "../../components/footers/footer";
+// import Text from "../../components/Typed/typed";
 const home = () => {
   return (
     <section className="section">
@@ -8,6 +9,7 @@ const home = () => {
       {/* <div className="lg:-mt-0 mt-10">
         <Footer/>
       </div> */}
+      {/* <Text/> */}
     </section>
   );
 };

7186fe49e5063e2d7f9994d82ce9c7805ea545bd
git-create-devben <DevBen.tech@outlook.com>
2023-12-15 19:04:31 +0100

    update

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -4,9 +4,9 @@ import "./home.css";
 // import Text from "../../components/Typed/typed";
 const home = () => {
   return (
-    <section className="section flex flex-col justify-center lg:pl-48 pl-10 lg:pt-0 pt-48 -space-y-3">
+    <section className="section flex flex-col justify-center lg:pl-48 pl-6 lg:pt-0 pt-48 -space-y-3">
       <span className="text-4xl text-[#e49974] opacity-50">I&apos;m</span>
-      <h1 className="lg:text-[8rem] tsm:text-5xl text-7xl text-[#D0B8AC]">
+      <h1 className="lg:text-[8rem] sm:text-5xl text-7xl text-[#D0B8AC]">
         Christina <br /> Demertzi.
       </h1>
       <br />
